03279d0bca344fb8eac4b74cea6501b897ed0d90
SamN123 <143456414+SamN123@users.noreply.github.com>
2024-12-07 20:02:17 -0500

    Update BinaryTree.cpp
    Added comments to describe main functions

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -4,10 +4,11 @@
 class BinaryTree {
 public:
     Node* rootNode;
+    // Constructor for the Binary Tree Class with rootNode initialized when the object is constructed
     explicit BinaryTree(Node* head){
         rootNode = sortedBST(head);
     }
-
+    // In Order traversal of the binary tree with sorted output 
     void inOrder(Node* current){
         if (current == nullptr){
             return;
@@ -16,6 +17,7 @@ public:
         std::cout << current->key << " ";
         inOrder(current->right);
     }
+    // Post Order traversal of the binary tree with sorted output 
     void postOrder(Node *current){
         if (current == nullptr){
             return;
@@ -24,6 +26,7 @@ public:
         postOrder(current->right);
         std::cout << current->key << " ";
     }
+    // Pre Order traversal of the binary tree with sorted output 
     void preOrder(Node *current){
         if (current == nullptr){
             return;
@@ -32,7 +35,7 @@ public:
         preOrder(current->left);
         preOrder(current->right);
     }
-    
+    // Finds and returns the middle node in linked list using fast and slow pointers 
     static Node* findMiddle(Node *&head){
         Node *fast = head;
         Node *slow = head;
@@ -47,6 +50,7 @@ public:
         if (prev != nullptr) prev->flag=true;
         return slow;
     }
+    // Recursive conversion of a sorted link list into a Binary Search Tree 
     static Node* sortedBST(Node *&head) {
         if (head == nullptr){
             return nullptr;
@@ -64,11 +68,12 @@ public:
         root->right = sortedBST(mid->next);
         return root;
     }
-
+    // Displays the post order format of the Binary Tree  
     void printPostOrder(){
         postOrder(rootNode);
         std::cout << std::endl;
     }
+    // Searches for a specific node with a given key using recursion 
     Node* searchNode(Node* rootNode, unsigned long int key2){
         if (rootNode == nullptr || rootNode->key == key2){
             return rootNode;
@@ -79,6 +84,7 @@ public:
         return searchNode(rootNode->left, key2);
     }
 };
+// Returns the length of the Binary Tree 
 int getLength(Node *head){
     int length = 0;
     while (head != nullptr){
