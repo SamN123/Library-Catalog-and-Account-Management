1d68e27349c6cb969b67ce6885edef12aa82b302
wilsonrost <30719487+wilsonrost@users.noreply.github.com>
2024-12-08 17:29:41 -0500

    Update Library.cpp

--- Library.cpp
+++ Library.cpp
@@ -1,14 +1,18 @@
-#include <algorithm>
 #include <string>
-#include <vector>
 #include <fstream>
 #include <sstream>
 
 #include "LinkedList.cpp"
 
+using namespace std;
+
 class Library {
 
 public:
+
+    Node* traverse;
+    int counter = 1;
+    int width = 0;
 
     Library(LinkedList& list1) {
         std::ifstream inputFile("expanded_book_titles.txt");
@@ -18,21 +22,34 @@ public:
 
         std::string line;
         while (getline(inputFile, line)) {
-        std::stringstream ss(line); // Load 'line' into the string stream
-        std::string title; 
-        std::string dateStr;
-        int date; 
+            std::stringstream ss(line); // Load 'line' into the string stream
+            std::string title; 
+            std::string dateStr;
+            int date; 
         
-        // Split the line by the pipe delimiter
-        if (getline(ss, title, '|') && getline(ss, dateStr, ';')) {
-            // Convert the date string to an integer
-            date = stoi(dateStr);
-        };
+            // Split the line by the pipe delimiter
+            if (getline(ss, title, '|') && getline(ss, dateStr, ';')) {
+                // Convert the date string to an integer
+                date = stoi(dateStr);
+            };
         //cout << "Title: " << title << ", Due date: " << date << endl;
-        list1.addNode(title, date);
+            list1.addNode(title, date);
+        };
+
+       
+
+        Node* traverse = list1.getHead();
+        while(traverse != nullptr) {
+            traverse->key = hashFunction(traverse->title);
+            //cout << hex << traverse->key << endl;
+            traverse = traverse->next;
+            counter++;
+            if(traverse!=nullptr && traverse->title.length() > width) width = traverse->title.length(); 
+
+        }
+
     };
-    //list1.printList();
-    };
+
     unsigned long int hashFunction(const std::string& entry) { //generates value based on 
          unsigned long int hash = 0;
          
@@ -41,28 +58,17 @@ public:
             hash = i == 7 ? hash : hash << 8; //bit-shift hash left 8 bits to make space for next character
         } 
         
-    return hash;
-        };
+        return hash;
+    };
 
     void saveCatalog(Node* head) {
         
         std::ofstream outputFile("expanded_book_titles.txt");
         while(head != nullptr){
-        outputFile << head->title <<"|" << head->date << ";";
-        if(head->next != nullptr) outputFile << "\n";
-        head = head->next;
+            outputFile << head->title <<"|" << head->date << ";";
+            if(head->next != nullptr) outputFile << "\n";
+            head = head->next;
         }
         outputFile.close();
-    }
-
-    
-
-	//void saveCatalog(); // save catalog to the file
-	//void loadClientList(); // loads book checked out text file 
-	//void saveClientList(); // saves checked books into text file 
-	// void sortList(); // ** function to use sort function (binary search tree?) 
-	// void searchCatalog();
-	// void borrowBook(); // adding customer to borrowed books text file 
-	// void returnBook(); // removing customer from borrowed books text file 
-	
+    }	
 };
